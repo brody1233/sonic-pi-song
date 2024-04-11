392ffabca28b4b8f31866c9a049366b16878fa59
brody1233 <152130896+brody1233@users.noreply.github.com>
2024-04-11 11:31:31 -0700

    Update finalsong.rb

--- finalsong.rb
+++ finalsong.rb
@@ -1,3 +1,4 @@
+use_bpm 120
 define :restmeasure do
   play :r
   sleep 1
@@ -11,15 +12,21 @@ define :restmeasure do
   sleep 1
 end
 
-define :bassfnct do |i1,i2,i3,i4|
+low = [:g2, :c3]
+
+define :measure6 do |i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12|
   play i1
   sleep i2
-  play i1
-  sleep i3
-  play i1
-  sleep i3
-  play i1
+  play i3
   sleep i4
+  play i5
+  sleep i6
+  play i7
+  sleep i8
+  play i9
+  sleep i10
+  play i11
+  sleep i12
 end
 live_loop :drum do
   32.times do
@@ -31,61 +38,19 @@ sleep 16
 live_loop :voice do
   use_synth :organ_tonewheel
   restmeasure
-  play :b4
-  sleep 1
-  play :c5
-  sleep 0.5
-  play :b4
-  sleep 1
-  play :a4
-  sleep 0.5
-  play :g4
-  sleep 0.5
-  play :a4
-  sleep 0.5
+  measure6 :b4,1,:c5,0.5,:b4,1,:a4,0.5,:g4,0.5,:a4,0.5
   
-  play :b4
-  sleep 1
-  play :c5
-  sleep 0.5
-  play :b4
-  sleep 1
-  play :a4
-  sleep 0.5
-  play :g4
-  sleep 0.5
-  play :a4
-  sleep 0.5
+  measure6 :b4,1,:c5,0.5,:b4,1,:a4,0.5,:g4,0.5,:a4,0.5
+  
   
   play :b4, sustain: 4
   sleep 4
   
   restmeasure
-  play :b4
-  sleep 1
-  play :c5
-  sleep 0.5
-  play :b4
-  sleep 1
-  play :a4
-  sleep 0.5
-  play :g4
-  sleep 0.5
-  play :a4
-  sleep 0.5
+  measure6 :b4,1,:c5,0.5,:b4,1,:a4,0.5,:g4,0.5,:a4,0.5
+  measure6 :b4,1,:c5,0.5,:b4,1,:a4,0.5,:g4,0.5,:a4,0.5
   
-  play :b4
-  sleep 1
-  play :c5
-  sleep 0.5
-  play :b4
-  sleep 1
-  play :a4
-  sleep 0.5
-  play :g4
-  sleep 0.5
-  play :a4
-  sleep 0.5
+  measure6 :b4,1,:c5,0.5,:b4,1,:a4,0.5,:g4,0.5,:a4,0.5
   
   play :b4, sustain: 1
   sleep 2
@@ -96,11 +61,34 @@ sleep 16
 #bass
 live_loop :bass do
   use_synth :bass_foundation
-  bassfnct :g2,1.5,1,0.5
-  bassfnct :c3,0.5,1,1.5
-  bassfnct :g2,0.5,1,1.5
+  play :g2
+  sleep 1.5
+  play :g2
+  sleep 1
+  play :g2
+  sleep 1
+  play :g2
+  sleep 0.5
   
+  play :c3
+  sleep 1.5
+  play :c3
+  sleep 1
+  play :c3
+  sleep 0.5
+  play :c3
+  sleep 1.5
+  
+  play :c3
+  sleep 1
+  play :c3
+  sleep 1
+  play :c3
+  sleep 1
+  play :c3
+  sleep 1
   
   play :r
   sleep 4
 end
+stop
