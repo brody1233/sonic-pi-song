b305cf3ed8f53945f91be58442509578e08dc496
brody1233 <152130896+brody1233@users.noreply.github.com>
2024-04-11 11:39:59 -0700

    Update finalsong.rb

--- finalsong.rb
+++ finalsong.rb
@@ -28,56 +28,59 @@ define :measure6 do |i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12|
   play i11
   sleep i12
 end
-live_loop :drum do
-  32.times do
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 0.5
+1.times do
+  live_loop :drum do
+    32.times do
+      sample :drum_cymbal_closed, amp: 0.5
+      sleep 0.5
+    end
   end
-end
-sleep 16
-live_loop :voice do
-  use_synth :organ_tonewheel
-  restmeasure
-  2.times do
-    measure6 :b4,1,:c5,0.5,:b4,1,:a4,0.5,:g4,0.5,:a4,0.5
+  sleep 16
+  live_loop :voice do
+    use_synth :organ_tonewheel
+    restmeasure
+    2.times do
+      measure6 :b4,1,:c5,0.5,:b4,1,:a4,0.5,:g4,0.5,:a4,0.5
+    end
+    
+    play :b4, sustain: 4
+    sleep 4
+    
+    restmeasure
+    3.times do
+      measure6 :b4,1,:c5,0.5,:b4,1,:a4,0.5,:g4,0.5,:a4,0.5
+    end
+    
+    play :b4, sustain: 1
+    sleep 2
+    play :c5, sustain: 1
+    sleep 2
   end
-  
-  play :b4, sustain: 4
-  sleep 4
-  
-  restmeasure
-  3.times do
-    measure6 :b4,1,:c5,0.5,:b4,1,:a4,0.5,:g4,0.5,:a4,0.5
-  end
-  
-  play :b4, sustain: 1
-  sleep 2
-  play :c5, sustain: 1
-  sleep 2
-end
-sleep 16
-#bass
-live_loop :bass do
-  use_synth :bass_foundation
-  play low[0]
-  sleep low[3]
-  play low[0]
-  sleep low[2]
-  play low[0]
-  sleep low[2]
-  play low[0]
-  sleep low[4]
-  2.times do
-    play low[1]
+  sleep 16
+  #bass
+  live_loop :bass do
+    use_synth :bass_foundation
+    play low[0]
     sleep low[3]
-    play low[1]
+    play low[0]
     sleep low[2]
-    play low[1]
+    play low[0]
     sleep low[2]
-    play low[1]
+    play low[0]
     sleep low[4]
-   
+    2.times do
+      play low[1]
+      sleep low[3]
+      play low[1]
+      sleep low[2]
+      play low[1]
+      sleep low[2]
+      play low[1]
+      sleep low[4]
+      
+    end
+    play :r
+    sleep 4
   end
-  play :r
-  sleep 4
 end
+stop
